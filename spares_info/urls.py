4172ccea16baab49e4fdcabc9fb2021fdcaca455
aq1 <inmosh@yandex.ru>
2015-11-23 00:58:44 +0300

    Fileupload

--- spares_info/urls.py
+++ spares_info/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'shops/(?P<pk>[0-9]+)/?$', views.shops, name='shops'),
     url(r'shops/?$', views.shops, name='shops'),
     url(r'catalog/?$', views.catalog, name='catalog'),
+    url(r'catalog_upload/?$', views.catalog_upload, name='catalog_upload'),
+    url(r'utils/?$', views.utils, name='utils'),
     url(r'/?$', views.index, name='index'),
 ]
--- spares_info/views.py
+++ spares_info/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, redirect
 from django.core.urlresolvers import reverse
+from django.template import RequestContext
 
 from spares_info import models
 
@@ -19,6 +20,11 @@ def contacts(request):
     return render_to_response('spares_info/contacts.html')
 
 
+def catalog(request):
+
+    return render_to_response('spares_info/catalog.html')
+
+
 def shops(request, pk=None):
 
     if not pk:
@@ -36,6 +42,43 @@ def shops(request, pk=None):
     return render_to_response(template, context)
 
 
-def catalog(request):
 
-    return render_to_response('spares_info/catalog.html')
+def n(f):
+    import csv
+    import re
+    import time
+
+    j = 0
+    r = re.compile('\d')
+    ff = csv.reader(f.read().decode('cp1251'))
+    for i, row in enumerate(ff):
+        print(row)
+        try:
+            row[2]
+        except IndexError:
+            continue
+        if not row[2] and row[1]:
+            category = row[1]
+        else:
+            rr = r.findall(row[2])
+            price = '%s.%s' % (''.join(rr[:-2]), ''.join(rr[-2:]))
+            try:
+                price = float(price)
+            except ValueError:
+                j += 1
+                print('WOWOWOWOW', i, row)
+
+            print(i, category, row[1], price)
+
+# @login_reqired
+def catalog_upload(request):
+    n(request.FILES['file'])
+
+    return render_to_response('spares_info/catalog_upload_result.html',
+                              {'r': request.FILES['file']})
+
+
+def utils(request):
+
+    return render_to_response('spares_info/utils.html',
+                              context_instance=RequestContext(request))
